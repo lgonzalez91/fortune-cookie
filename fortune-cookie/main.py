4deb23c60fbdf4e4a688433764c730ede3dd6218
L Gonzalez <luisalbertogonz91@gmail.com>
2017-01-23 15:50:25 -0600

    Add fortunes and cookie_again_button function

--- fortune-cookie/main.py
+++ fortune-cookie/main.py
@@ -17,15 +17,44 @@
 import webapp2
 import random
 
+def getRandomFortune():
+    fortunes = [
+        "I see much code in your future",
+        "Consider eating more fortune cookies",
+        "You have tamed the mighty Python, now you must free it onto the great spider's web.",
+    ]
+
+    index = random.randint(0,2)
+
+    return fortunes[index]
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        header = '<h1>Fortune Cookie</h1>'
-        lucky_number = random.randint(0,100)
-        number_sentence = 'Your lucky number: ' + str(lucky_number)
-        number_paragraph = "<p>" + number_sentence+ "</p>"
-        self.response.write(header + number_paragraph)
 
+        header = '<h1>Fortune Cookie</h1>'
+
+        fortune = "<strong>"+getRandomFortune()+"</strong>"
+        fortune_sentence = "Your fortune: " + fortune
+        fortune_paragraph = "<p>" + fortune_sentence + "</p>"
+
+
+        lucky_number = "<strong>"+str(random.randint(0,100))+"</strong>"
+        number_sentence = 'Your lucky number: ' + lucky_number
+        number_paragraph = "<p>" + number_sentence+ "</p>"
+
+        cookie_again_button = "<a href='.''><button>Another cookie please!</button></a>"
+
+        content = header + fortune_paragraph + number_paragraph+cookie_again_button
+
+        self.response.write(content)
+
+
+
+
+class LoginHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write("Thanks for trying to log in!")
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
 ], debug=True)
