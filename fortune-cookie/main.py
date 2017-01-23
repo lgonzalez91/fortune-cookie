b1df2b088a3e4d9810bad4c278203227c48052b3
L Gonzalez <luisalbertogonz91@gmail.com>
2017-01-23 15:03:11 -0600

    Added the random number feature

--- fortune-cookie/main.py
+++ fortune-cookie/main.py
@@ -15,10 +15,16 @@
 # limitations under the License.
 #
 import webapp2
+import random
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello worldlololol!')
+        header = '<h1>Fortune Cookie</h1>'
+        lucky_number = random.randint(0,100)
+        number_sentence = 'Your lucky number: ' + str(lucky_number)
+        number_paragraph = "<p>" + number_sentence+ "</p>"
+        self.response.write(header + number_paragraph)
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
